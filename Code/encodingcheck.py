70203acae40dece545f90b5868e563ee72592896
iobio-zjut <43274315+iobio-zjut@users.noreply.github.com>
2025-07-31 10:42:01 +0800

    Delete Code directory

--- Code/encodingcheck.py
+++
@@ -1,8 +0,0 @@
-import chardet
-
-with open('/home/data/user/lvzexin/zexinl/abag_data/preprocess/cov.csv', 'rb') as f:
-    print(f.readline())
-    print(f.readline())
-    result = chardet.detect(f.read())  # 读取一定量的数据进行编码检测
-
-print(result['encoding'])  # 打印检测到的编码
--- Code/run_predict_sabdab.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-python eval_sabdab_abag.py --config /home/data/user/lvzexin/zexinl/MultiSAAI/Code/config/common/eval_abag.json
